a3d644a10ed301c5f76b4d18d472426a574f3d51
luisqv2006-web <luisqv2006@gmail.com>
2025-11-19 22:40:43 -0600

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,11 +5,10 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "CryptoSniper KeepAlive Running"
+    return "Bot activo."
 
 def run():
     app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
-    t = Thread(target=run)
-    t.start()
+    Thread(target=run).start()
