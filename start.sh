275d278cfb156893ff713596ff603f50b0f6801f
luisqv2006-web <luisqv2006@gmail.com>
2025-11-24 23:19:05 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-echo "🔁 Iniciando bot con reinicio automático..."
-
-while true
-do
-  python3 main.py
-  echo "⚠ Bot se detuvo — Reiniciando en 3 segundos..."
-  sleep 3
-done
+echo "Iniciando CryptoSniper FX v8.3..."
+python3 main.py
