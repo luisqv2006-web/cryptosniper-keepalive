387683c92b6e5ee138850c6613bd9e67302ddb47
luisqv2006-web <luisqv2006@gmail.com>
2025-11-25 08:30:01 -0600

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,12 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "CryptoSniper FX activo."
+    return "CryptoSniper FX Running"
 
 def run():
     app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
-    t = Thread(target=run)
-    t.daemon = True
-    t.start()
+    Thread(target=run).start()
