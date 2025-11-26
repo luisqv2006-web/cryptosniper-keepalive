36e9a2ce72f234492e8daaf15c6fa1a69c2a1d6a
luisqv2006-web <luisqv2006@gmail.com>
2025-11-25 19:52:42 -0600

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,16 +1,22 @@
-# ================================
-# 🔥 FIREBASE CONFIG
-# ================================
-
 import firebase_admin
 from firebase_admin import credentials, firestore
-import os
 
-# IMPORTANTE:
-# Descarga tu archivo JSON de credenciales desde Firebase
-# y súbelo a Render con el nombre: firebase_key.json
+# =========================================
+# CONFIGURACIÓN FIREBASE
+# =========================================
 
-cred = credentials.Certificate("firebase_key.json")
+cred = credentials.Certificate({
+    "type": "service_account",
+    "project_id": "TU_PROJECT_ID",
+    "private_key_id": "TU_PRIVATE_KEY_ID",
+    "private_key": "TU_PRIVATE_KEY",
+    "client_email": "TU_CLIENT_EMAIL",
+    "client_id": "TU_CLIENT_ID",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "TU_CLIENT_CERT_URL"
+})
+
 firebase_admin.initialize_app(cred)
-
 db = firestore.client()
