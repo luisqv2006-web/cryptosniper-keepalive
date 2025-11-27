c9b145f345dcfb0e10703754e34ae3493d783061
luisqv2006-web <luisqv2006@gmail.com>
2025-11-26 21:20:48 -0600

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -2,11 +2,17 @@ import os
 import firebase_admin
 from firebase_admin import credentials, firestore
 
+private_key = os.getenv("FIREBASE_PRIVATE_KEY")
+client_email = os.getenv("FIREBASE_CLIENT_EMAIL")
+
+if not private_key or not client_email:
+    raise ValueError("❌ Credenciales de Firebase NO configuradas en Render.")
+
 cred = credentials.Certificate({
     "type": "service_account",
     "project_id": "cryptosniper-keepalive-1",
-    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
-    "client_email": "firebase-adminsdk-fbsvc@cryptosniper-keepalive-1.iam.gserviceaccount.com",
+    "private_key": private_key.replace("\\n", "\n"),
+    "client_email": client_email,
     "token_uri": "https://oauth2.googleapis.com/token",
 })
 
