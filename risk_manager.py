e6c3c8640c1a8ab185e0f572163694a20cf963c9
luisqv2006-web <luisqv2006@gmail.com>
2025-11-27 00:54:42 -0600

    Update risk_manager.py

--- risk_manager.py
+++ risk_manager.py
@@ -3,18 +3,33 @@ class RiskManager:
         self.balance_inicial = balance_inicial
         self.max_loss_day = max_loss_day
         self.max_trades_day = max_trades_day
-        self.trades = 0
-        self.losses = 0
 
-    def registrar_perdida(self):
-        self.losses += 1
-
-    def registrar_trade(self):
-        self.trades += 1
+        self.perdidas_hoy = 0
+        self.trades_hoy = 0
+        self.racha_perdidas = 0
+        self.pausado = False
 
     def puede_operar(self):
-        if self.losses >= self.max_loss_day:
+        if self.pausado:
             return False
-        if self.trades >= self.max_trades_day:
+        if self.perdidas_hoy >= self.max_loss_day:
+            return False
+        if self.trades_hoy >= self.max_trades_day:
             return False
         return True
+
+    def registrar_trade(self):
+        self.trades_hoy += 1
+
+    def registrar_perdida(self):
+        self.perdidas_hoy += 1
+        self.racha_perdidas += 1
+
+        if self.racha_perdidas == 2:
+            self.pausado = True  # Pausa temporal
+        if self.racha_perdidas >= 3:
+            self.pausado = True  # Cierre total
+
+    def registrar_win(self):
+        self.racha_perdidas = 0
+        self.pausado = False
