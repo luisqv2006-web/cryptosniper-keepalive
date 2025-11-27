416949a59a365794959694d8547bfb9c45fad912
luisqv2006-web <luisqv2006@gmail.com>
2025-11-26 21:13:58 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-echo "🚀 Iniciando CryptoSniper FX en modo GRATIS..."
+echo "🚀 Iniciando CryptoSniper FX en modo DEBUG..."
 
-python3 main.py &
+echo "▶ Ejecutando main.py..."
+python3 main.py
+
+echo "▶ Ejecutando keep_alive.py..."
 python3 keep_alive.py
