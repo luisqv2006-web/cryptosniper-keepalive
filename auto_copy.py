45bedd81c7ec8dfceeaa0bc72c664bf3ca762f72
luisqv2006-web <luisqv2006@gmail.com>
2025-11-27 00:55:21 -0600

    Update auto_copy.py

--- auto_copy.py
+++ auto_copy.py
@@ -1,8 +1,8 @@
 class AutoCopy:
-    def __init__(self, token, stake=1, duration=5):
+    def __init__(self, token, stake=1, duration=1):
         self.token = token
         self.stake = stake
         self.duration = duration
 
     def ejecutar(self, symbol, direction):
-        print(f"[AUTOCOPY] Ejecutando {direction} en {symbol} con ${self.stake} por {self.duration}m")
+        print(f"[AutoCopy REAL] {direction} en {symbol} por ${self.stake} a {self.duration}m")
