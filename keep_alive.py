68835f741e0d60bc0eec030736c4045457e6ca17
luisqv2006-web <luisqv2006@gmail.com>
2026-04-29 19:45:02 -0600

    Fix threading issue in keep_alive function

--- keep_alive.py
+++ keep_alive.py
@@ -9,7 +9,6 @@ def home():
     return "Bot is alive and monitoring Deriv!"
 
 def run():
-    # Render usa la variable de entorno PORT, por defecto es 10000
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
 
