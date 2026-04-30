561aa9def909a3295c2a213c6b085f2bb64f2b29
luisqv2006-web <luisqv2006@gmail.com>
2026-04-29 19:41:43 -0600

    Delete auto_copy.py

--- auto_copy.py
+++
@@ -1,8 +0,0 @@
-class AutoCopy:
-    def __init__(self, token, stake=1, duration=1):
-        self.token = token
-        self.stake = stake
-        self.duration = duration
-
-    def ejecutar(self, symbol, direction):
-        print(f"[AutoCopy REAL] {direction} en {symbol} por ${self.stake} a {self.duration}m")
